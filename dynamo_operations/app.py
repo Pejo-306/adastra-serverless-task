5b49f5698fd5bbdd50aa996e42690b3ec4ee5ab1
Petar Nikolov <penikolov23@gmail.com>
2021-08-06 17:34:10 +0300

    Implement read operation to DynamoDB table

--- dynamo_operations/app.py
+++ dynamo_operations/app.py
@@ -5,7 +5,42 @@ from typing import Dict, Any
 import boto3
 
 
+def read_from_db(table: 'boto3.resources.factory.dynamodb.Table',
+                 event: Dict[str, Any]) -> Dict[str, Any]:
+    item_pk = event['queryStringParameters']['id']
+    table_response = table.get_item(Key={'id': item_pk})
+    if 'Item' not in table_response.keys():
+        # TODO: return error here
+        pass
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'table': table.table_name,
+            'item': table_response['Item']
+        }),
+    }
+
+
+def insert_into_db(table: 'boto3.resources.factory.dynamodb.Table',
+                   event: Dict[str, Any]) -> Dict[str, Any]:
+    payload = json.loads(event['body'])['payload']['Item']
+    table.put_item(Item=payload)
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'table': table.table_name,
+            'item_primary_key': payload['id']
+        }),
+    }
+
+
 def lambda_handler(event, context) -> Dict[str, Any]:
+    operations = {
+        'read': read_from_db,
+        'insert': insert_into_db,
+    }
+
     # Connect to the test DynamoDB table
     table_name = os.environ.get('TABLE_NAME')
     dynamodb = boto3.resource('dynamodb')
@@ -13,13 +48,5 @@ def lambda_handler(event, context) -> Dict[str, Any]:
 
     # Insert the item into the database table
     data = json.loads(event['body'])
-    payload = data['payload']['Item']
-    table.put_item(Item=payload)
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "table": table_name,
-            'item_primary_key': payload['id']
-        }),
-    }
+    response = operations[data['operation']](table, event)
+    return response
