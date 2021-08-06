3cb6925307022f5663f383493ba478896a142e28
Petar Nikolov <penikolov23@gmail.com>
2021-08-06 17:10:00 +0300

    Add unit tests for DynamoDB read event

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -186,6 +186,64 @@ def apigw_insert_event() -> Dict[str, Any]:
 
 
 @pytest.fixture()
+def apigw_read_event() -> Dict[str, Any]:
+    body = {
+        "operation": "read",
+    }
+    return {
+        "body": json.dumps(body),
+        "resource": "/{proxy+}",
+        "requestContext": {
+            "resourceId": "123456",
+            "apiId": "1234567890",
+            "resourcePath": "/{proxy+}",
+            "httpMethod": "POST",
+            "requestId": "c6af9ac6-7b61-11e6-9a41-93e8deadbeef",
+            "accountId": "123456789012",
+            "identity": {
+                "apiKey": "",
+                "userArn": "",
+                "cognitoAuthenticationType": "",
+                "caller": "",
+                "userAgent": "Custom User Agent String",
+                "user": "",
+                "cognitoIdentityPoolId": "",
+                "cognitoIdentityId": "",
+                "cognitoAuthenticationProvider": "",
+                "sourceIp": "127.0.0.1",
+                "accountId": "",
+            },
+            "stage": "prod",
+        },
+        "queryStringParameters": {"id": "1234567890"},
+        "headers": {
+            "Via": "1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)",
+            "Accept-Language": "en-US,en;q=0.8",
+            "CloudFront-Is-Desktop-Viewer": "true",
+            "CloudFront-Is-SmartTV-Viewer": "false",
+            "CloudFront-Is-Mobile-Viewer": "false",
+            "X-Forwarded-For": "127.0.0.1, 127.0.0.2",
+            "CloudFront-Viewer-Country": "US",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Upgrade-Insecure-Requests": "1",
+            "X-Forwarded-Port": "443",
+            "Host": "1234567890.execute-api.us-east-1.amazonaws.com",
+            "X-Forwarded-Proto": "https",
+            "X-Amz-Cf-Id": "aaaaaaaaaae3VYQb9jd-nvCd-de396Uhbp027Y2JvkCPNLmGJHqlaA==",
+            "CloudFront-Is-Tablet-Viewer": "false",
+            "Cache-Control": "max-age=0",
+            "User-Agent": "Custom User Agent String",
+            "CloudFront-Forwarded-Proto": "https",
+            "Accept-Encoding": "gzip, deflate, sdch",
+        },
+        "pathParameters": {"proxy": "/examplepath"},
+        "httpMethod": "GET",
+        "stageVariables": {"baz": "qux"},
+        "path": "/records",
+    }
+
+
+@pytest.fixture()
 def table_name() -> str:
     name = os.environ.get('TABLE_NAME')
     if name is None:
@@ -222,6 +280,43 @@ def test_lambda_handler_with_insert_event(apigw_insert_event: Dict[str, Any],
         assert item['ts'] == '2021-08-06 14:43:23.687000'
         assert 'name' in item.keys()
         assert item['name'] == 'test_item'
+    finally:
+        # Ensure the test item is deleted
+        table.delete_item(
+            Key={'id': '1234567890'},
+            ConditionExpression='attribute_exists(id)'
+        )
+
+
+def test_lambda_handler_with_read_event(apigw_read_event: Dict[str, Any],
+                                        table_name: str) -> None:
+    # Connect to the test DynamoDB table
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(table_name)
+    # Preemptively create the test item
+    try:
+        table.put_item(Item={
+            'id': '1234567890',
+            'name': 'test_item'
+        })
+        table_response = table.get_item(Key={'id': '1234567890'})
+        assert 'Item' in table_response.keys()
+        item = table_response['Item']
+        assert 'id' in item.keys()
+        assert item['id'] == '1234567890'
+        assert 'name' in item.keys()
+        assert item['name'] == 'test_item'
+
+        response = app.lambda_handler(apigw_read_event, None)
+        data = json.loads(response['body'])
+        assert response['statusCode'] == 200
+        assert 'table' in response['body']
+        assert data['table'] == table_name
+        assert 'item' in response['body']
+        assert 'id' in data['item'].keys()
+        assert data['item']['id'] == '1234567890'
+        assert 'name' in data['item'].keys()
+        assert data['item']['name'] == 'test_item'
     finally:
         # Ensure the test item is deleted
         table.delete_item(
