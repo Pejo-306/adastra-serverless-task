abdf434960fbba310d849d12669b8e58f29395d7
Petar Nikolov <penikolov23@gmail.com>
2021-08-06 15:58:46 +0300

    Implement insert operation to DynamoDB table

--- dynamo_operations/app.py
+++ dynamo_operations/app.py
@@ -1,42 +1,25 @@
+import os
 import json
+from typing import Dict, Any
 
-# import requests
+import boto3
 
 
-def lambda_handler(event, context):
-    """Sample pure Lambda function
+def lambda_handler(event, context) -> Dict[str, Any]:
+    # Connect to the test DynamoDB table
+    table_name = os.environ.get('TABLE_NAME')
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(table_name)
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
+    # Insert the item into the database table
+    data = json.loads(event['body'])
+    payload = data['payload']['Item']
+    table.put_item(Item=payload)
 
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
+            "table": table_name,
+            'item_primary_key': payload['id']
         }),
     }
--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,6 +1,9 @@
 import json
+import os
+from typing import Dict, Any
 
 import pytest
+import boto3
 
 from dynamo_operations import app
 
@@ -62,12 +65,166 @@ def apigw_event():
     }
 
 
-def test_lambda_handler(apigw_event, mocker):
+@pytest.fixture()
+def apigw_get_event() -> Dict[str, Any]:
+    return {
+        "body": '{ "test": "body"}',
+        "resource": "/{proxy+}",
+        "requestContext": {
+            "resourceId": "123456",
+            "apiId": "1234567890",
+            "resourcePath": "/{proxy+}",
+            "httpMethod": "POST",
+            "requestId": "c6af9ac6-7b61-11e6-9a41-93e8deadbeef",
+            "accountId": "123456789012",
+            "identity": {
+                "apiKey": "",
+                "userArn": "",
+                "cognitoAuthenticationType": "",
+                "caller": "",
+                "userAgent": "Custom User Agent String",
+                "user": "",
+                "cognitoIdentityPoolId": "",
+                "cognitoIdentityId": "",
+                "cognitoAuthenticationProvider": "",
+                "sourceIp": "127.0.0.1",
+                "accountId": "",
+            },
+            "stage": "prod",
+        },
+        "queryStringParameters": {"foo": "bar"},
+        "headers": {
+            "Via": "1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)",
+            "Accept-Language": "en-US,en;q=0.8",
+            "CloudFront-Is-Desktop-Viewer": "true",
+            "CloudFront-Is-SmartTV-Viewer": "false",
+            "CloudFront-Is-Mobile-Viewer": "false",
+            "X-Forwarded-For": "127.0.0.1, 127.0.0.2",
+            "CloudFront-Viewer-Country": "US",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Upgrade-Insecure-Requests": "1",
+            "X-Forwarded-Port": "443",
+            "Host": "1234567890.execute-api.us-east-1.amazonaws.com",
+            "X-Forwarded-Proto": "https",
+            "X-Amz-Cf-Id": "aaaaaaaaaae3VYQb9jd-nvCd-de396Uhbp027Y2JvkCPNLmGJHqlaA==",
+            "CloudFront-Is-Tablet-Viewer": "false",
+            "Cache-Control": "max-age=0",
+            "User-Agent": "Custom User Agent String",
+            "CloudFront-Forwarded-Proto": "https",
+            "Accept-Encoding": "gzip, deflate, sdch",
+        },
+        "pathParameters": {"proxy": "/examplepath"},
+        "httpMethod": "GET",
+        "stageVariables": {"baz": "qux"},
+        "path": "/records/id=42",
+    }
 
-    ret = app.lambda_handler(apigw_event, "")
-    data = json.loads(ret["body"])
 
-    assert ret["statusCode"] == 200
-    assert "message" in ret["body"]
-    assert data["message"] == "hello world"
-    # assert "location" in data.dict_keys()
+@pytest.fixture()
+def apigw_insert_event() -> Dict[str, Any]:
+    body = {
+        "operation": "insert",
+        "payload": {
+            "Item": {
+                "id": "1234567890",
+                "ts": "2021-08-06 14:43:23.687000",
+                "name": "test_item"
+            }
+        }
+    }
+    return {
+        "body": json.dumps(body),
+        "resource": "/{proxy+}",
+        "requestContext": {
+            "resourceId": "123456",
+            "apiId": "1234567890",
+            "resourcePath": "/{proxy+}",
+            "httpMethod": "POST",
+            "requestId": "c6af9ac6-7b61-11e6-9a41-93e8deadbeef",
+            "accountId": "123456789012",
+            "identity": {
+                "apiKey": "",
+                "userArn": "",
+                "cognitoAuthenticationType": "",
+                "caller": "",
+                "userAgent": "Custom User Agent String",
+                "user": "",
+                "cognitoIdentityPoolId": "",
+                "cognitoIdentityId": "",
+                "cognitoAuthenticationProvider": "",
+                "sourceIp": "127.0.0.1",
+                "accountId": "",
+            },
+            "stage": "prod",
+        },
+        "queryStringParameters": {"foo": "bar"},
+        "headers": {
+            "Via": "1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)",
+            "Accept-Language": "en-US,en;q=0.8",
+            "CloudFront-Is-Desktop-Viewer": "true",
+            "CloudFront-Is-SmartTV-Viewer": "false",
+            "CloudFront-Is-Mobile-Viewer": "false",
+            "X-Forwarded-For": "127.0.0.1, 127.0.0.2",
+            "CloudFront-Viewer-Country": "US",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Upgrade-Insecure-Requests": "1",
+            "X-Forwarded-Port": "443",
+            "Host": "1234567890.execute-api.us-east-1.amazonaws.com",
+            "X-Forwarded-Proto": "https",
+            "X-Amz-Cf-Id": "aaaaaaaaaae3VYQb9jd-nvCd-de396Uhbp027Y2JvkCPNLmGJHqlaA==",
+            "CloudFront-Is-Tablet-Viewer": "false",
+            "Cache-Control": "max-age=0",
+            "User-Agent": "Custom User Agent String",
+            "CloudFront-Forwarded-Proto": "https",
+            "Accept-Encoding": "gzip, deflate, sdch",
+        },
+        "pathParameters": {"proxy": "/examplepath"},
+        "httpMethod": "POST",
+        "stageVariables": {"baz": "qux"},
+        "path": "/records",
+    }
+
+
+@pytest.fixture()
+def table_name() -> str:
+    name = os.environ.get('TABLE_NAME')
+    if name is None:
+        raise KeyError("Missing required environmental variable 'TABLE_NAME'")
+    return name
+
+
+def test_lambda_handler_with_insert_event(apigw_insert_event: Dict[str, Any],
+                                          table_name: str) -> None:
+    # Connect to the test DynamoDB table
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(table_name)
+    # Make sure the test item does not already exist in the table
+    table_response = table.get_item(Key={'id': '1234567890'})
+    assert 'Item' not in table_response.keys()
+
+    try:
+        # Execute the Lambda handler with the test event
+        response = app.lambda_handler(apigw_insert_event, None)
+        data = json.loads(response['body'])
+        assert response['statusCode'] == 200
+        assert 'table' in response['body']
+        assert data['table'] == table_name
+        assert 'item_primary_key' in response['body']
+        assert data['item_primary_key'] == '1234567890'
+
+        # Make sure the test item has been written
+        table_response = table.get_item(Key={'id': '1234567890'})
+        assert 'Item' in table_response.keys()
+        item = table_response['Item']
+        assert 'id' in item.keys()
+        assert item['id'] == '1234567890'
+        assert 'ts' in item.keys()
+        assert item['ts'] == '2021-08-06 14:43:23.687000'
+        assert 'name' in item.keys()
+        assert item['name'] == 'test_item'
+    finally:
+        # Ensure the test item is deleted
+        table.delete_item(
+            Key={'id': '1234567890'},
+            ConditionExpression='attribute_exists(id)'
+        )
